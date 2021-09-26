4bb8d508630a7a45d0e3f8d47dc98a41777001c5
SahanSamarakoon <sahan.18@cse.mrt.ac.lk>
2021-09-26 12:51:03 +0530

    First Commit 09/26/2021

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const { DB, PORT } = require("./config");
+
+const db = require("./models");
+const Role = db.role;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+require('./routes/authRoutes')(app);
+require('./routes/routes')(app);
+
+
+
+db.mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initialDbSetup();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+  app.listen(PORT, () => {
+    console.log("Server started on port:" + PORT);
+})
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome" });
+});
+
+function initialDbSetup() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("Added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "manager"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("Added 'manager' to roles collection");
+      });
+
+      new Role({
+        name: "salesperson"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("Added 'salesperson' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
